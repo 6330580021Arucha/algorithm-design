50a881a27d03ad92ac4aa30fad6d8fa32bd459ff
6330580021Arucha <oakbymarwin@gmail.com>
2024-06-15 23:46:50 +0700

    fix: mode & my_day_cover(test)

--- class/mode.cpp
+++ class/mode.cpp
@@ -26,7 +26,7 @@ pair<int, int> find_mode(vector<int> A, int left, int right)
     int n_mode = 0;
     for (auto a : map)
     {
-        if (a.second >= n_mode)
+        if (a.second > n_mode)
         {
             mode = a.first;
             n_mode = a.second;
@@ -49,26 +49,27 @@ pair<int, int> mode_with_half(vector<int> A, int left, int right)
         pair<int, int> m_left = mode_with_half(A, left, m);
         pair<int, int> m_right = mode_with_half(A, m + 1, right);
 
-        int mode = (m_left.second > m_right.second) ? m_left.first : m_right.first;
-        int n_mode = (m_left.second > m_right.second) ? m_left.second : m_right.second;
+        int mode = (m_left.second >= m_right.second) ? m_left.first : m_right.first;
+        int n_mode = (m_left.second >= m_right.second) ? m_left.second : m_right.second;
+        bool left = (m_left.second > m_right.second) ? true : false;
 
         if (A[m] == A[m + 1])
         {
             int m_mode = A[m];
-            int left = m;
-            int right = m + 1;
+            int m_left = m;
+            int m_right = m + 1;
 
-            while (A[left] == A[m])
-                left--;
-            left++;
-            while (A[right] == A[m + 1])
-                right++;
-            right--;
+            while (m_left >= left && A[m_left] == A[m])
+                m_left--;
+            m_left++;
+            while (m_right <= right && A[m_right] == A[m + 1])
+                m_right++;
+            m_right--;
 
-            int m_n_mode = right - left + 1;
+            int m_n_mode = m_right - m_left + 1;
 
-            mode = (n_mode >= m_n_mode) ? mode : m_mode;
-            n_mode = (n_mode >= m_n_mode) ? n_mode : m_n_mode;
+            mode = (n_mode > m_n_mode) ? mode : m_mode;
+            n_mode = (n_mode > m_n_mode) ? n_mode : m_n_mode;
         }
         return make_pair(mode, n_mode);
     }
@@ -89,8 +90,8 @@ pair<int, int> mode_with_half_2(vector<int> A, int left, int right)
             pair<int, int> ans1 = mode_with_half_2(A, left, m);
             pair<int, int> ans2 = mode_with_half_2(A, m + 1, right);
 
-            int mode = (ans1.second > ans2.second) ? ans1.first : ans2.first;
-            int n_mode = (ans1.second > ans2.second) ? ans1.second : ans2.second;
+            int mode = (ans1.second >= ans2.second) ? ans1.first : ans2.first;
+            int n_mode = (ans1.second >= ans2.second) ? ans1.second : ans2.second;
 
             return make_pair(mode, n_mode);
         }
@@ -100,16 +101,15 @@ pair<int, int> mode_with_half_2(vector<int> A, int left, int right)
             int m_left = m;
             int m_right = m;
 
-            while (tmp == A[m_left])
+            while (m_left >= left && tmp == A[m_left])
                 m_left--;
             if (m_left < left)
             {
-                while (tmp == A[m_right])
+                while (m_right <= right && tmp == A[m_right])
                     m_right++;
-                m_right--;
-                if (m_right == right)
+                if (m_right > right)
                     return make_pair(A[right], right - left + 1);
-                m = m_right;
+                m = m_right - 1;
             }
             else
             {
@@ -119,8 +119,8 @@ pair<int, int> mode_with_half_2(vector<int> A, int left, int right)
             pair<int, int> ans1 = mode_with_half_2(A, left, m);
             pair<int, int> ans2 = mode_with_half_2(A, m + 1, right);
 
-            int mode = (ans1.second > ans2.second) ? ans1.first : ans2.first;
-            int n_mode = (ans1.second > ans2.second) ? ans1.second : ans2.second;
+            int mode = (ans1.second >= ans2.second) ? ans1.first : ans2.first;
+            int n_mode = (ans1.second >= ans2.second) ? ans1.second : ans2.second;
 
             return make_pair(mode, n_mode);
         }
@@ -153,42 +153,84 @@ pair<int, int> mode_with_jump(vector<int> A, int left, int right)
         pair<int, int> ans_left = find_mode2(A, left, m);
         pair<int, int> ans_right = mode_with_jump(A, m + 1, right);
 
-        int mode = (ans_left.second > ans_right.second) ? ans_left.first : ans_right.first;
-        int n_mode = (ans_left.second > ans_right.second) ? ans_left.second : ans_right.second;
+        int mode = (ans_left.second >= ans_right.second) ? ans_left.first : ans_right.first;
+        int n_mode = (ans_left.second >= ans_right.second) ? ans_left.second : ans_right.second;
 
         return make_pair(mode, n_mode);
     }
 }
 
+vector<bool> test_function()
+{
+    vector<bool> result(4, true);
+    vector<vector<int>> test_cases = {
+        {},                                // 0 Empty vector
+        {1},                               // 1 Single element
+        {1, 1, 1, 1, 1},                   // 2 All elements the same
+        {1, 1, 1, 1, 1, 1},                // 3 All elements the same(even)
+        {1, 2},                            // 4 Two different elements
+        {2, 2, 2, 2, 2, 3, 3, 3, 3},       // 5 Two different elements with more duplicates
+        {1, 2, 3, 4, 5},                   // 6 Increasing sequence
+        {1, 1, 2, 5, 5},                   // 7 NEW
+        {1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3}, // 8 Mixed elements
+        {-3, -2, -1, 0, 1, 2, 3},          // 9 Vector with negative numbers
+        {1, 1, 2, 3, 3, 4, 5, 5, 6}        // 10 Vector with duplicates and unique elements
+    };
+
+    vector<pair<int, int>> expected_results = {
+        {0, 0},  // 0 Empty vector
+        {1, 1},  // 1 Single element
+        {1, 5},  // 2 All elements the same
+        {1, 6},  // 3 All elements the same(even)
+        {1, 1},  // 4 Two different elements
+        {2, 5},  // 5 Two different elements with more duplicates
+        {1, 1},  // 6 Increasing sequence
+        {1, 2},  // 7 NEW
+        {2, 4},  // 8 Mixed elements
+        {-3, 1}, // 9 Vector with negative numbers
+        {1, 2}   // 10 Vector with duplicates and unique elements
+    };
+
+    for (int i = 0; i < test_cases.size(); i++)
+    {
+        if (find_mode(test_cases[i], 0, test_cases[i].size() - 1) != expected_results[i])
+        {
+            pair<int, int> A = find_mode(test_cases[i], 0, test_cases[i].size() - 1);
+            printf("brute force : {%d, %d} (testcase: %d)\n", A.first, A.second, i);
+            result[0] = false;
+        }
+        if (mode_with_half(test_cases[i], 0, test_cases[i].size() - 1) != expected_results[i])
+        {
+            pair<int, int> A = mode_with_half(test_cases[i], 0, test_cases[i].size() - 1);
+            printf("divide : {%d, %d} (testcase: %d)\n", A.first, A.second, i);
+            result[1] = false;
+        }
+        if (mode_with_half_2(test_cases[i], 0, test_cases[i].size() - 1) != expected_results[i])
+        {
+            pair<int, int> A = mode_with_half_2(test_cases[i], 0, test_cases[i].size() - 1);
+            printf("divide2 : {%d, %d} (testcase: %d)\n", A.first, A.second, i);
+            result[2] = false;
+        }
+        if (mode_with_jump(test_cases[i], 0, test_cases[i].size() - 1) != expected_results[i])
+        {
+            pair<int, int> A = mode_with_jump(test_cases[i], 0, test_cases[i].size() - 1);
+            printf("divide_jump : {%d, %d} (testcase: %d)\n", A.first, A.second, i);
+            result[3] = false;
+        }
+    }
+
+    return result;
+}
+
 int main()
 {
-    // vector<int> A;
-    // vector<int> A = {1};
-    // vector<int> A = {1, 1};
-    // vector<int> A = {1, 1, 1};
-    // vector<int> A = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-    // vector<int> A = {0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8};
-    // vector<int> A = {0, 1, 2, 3, 4, 4, 4, 5, 6, 7, 8};
-    // vector<int> A = {0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8};
-    // vector<int> A = {-2, -1, 0, 1, 2};
-    // vector<int> A = {-2, -2, -1, 0, 1, 2};
-    // vector<int> A = {-2, -1, 0, 0, 1, 2};
-    vector<int> A = {1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3};
+    vector<bool> result = test_function();
 
-    pair<int, int> m = find_mode(A, 0, A.size() - 1);
-    pair<int, int> my_mode = mode_with_half(A, 0, A.size() - 1);
-    pair<int, int> mode_half = mode_with_half_2(A, 0, A.size() - 1);
-    pair<int, int> mode_jump = mode_with_jump(A, 0, A.size() - 1);
-
-    printf("mode_brute force = %d, frequently = %d\n", m.first, m.second);
-    printf("================================================\n\n");
-
-    printf("mode_divide = %d, frequently = %d\n", my_mode.first, my_mode.second);
-    printf("================================================\n\n");
-
-    printf("mode_divide_2 = %d, frequently = %d\n", mode_half.first, mode_half.second);
-    printf("================================================\n\n");
-
-    printf("mode_jump = %d, frequently = %d\n", mode_jump.first, mode_jump.second);
-    printf("================================================\n\n");
+    for (auto a : result)
+    {
+        if (a)
+            cout << "TRUE ";
+        else
+            cout << "FALSE ";
+    }
 }
